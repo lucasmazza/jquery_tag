eb2487f11c3d683b57e352f4a8d4a9ef720a6912
Lucas Mazza <luc4smazza@gmail.com>
2010-09-29 22:50:00 -0300

    Passing the options array forward

--- lib/jquery_tag/view_helpers.rb
+++ lib/jquery_tag/view_helpers.rb
@@ -4,9 +4,9 @@ module Jquery
       
       def jquery_tag(*options)
         if Rails.env.production?
-           javascript_include_tag "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"
+           javascript_include_tag "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js", options
          else
-           javascript_include_tag "jquery.js"
+           javascript_include_tag "jquery.js", options
         end
       end
     end

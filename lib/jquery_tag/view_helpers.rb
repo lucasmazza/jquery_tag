773d27d77dc66474fc2fec8ed0088d08518a6d29
Lucas Mazza <luc4smazza@gmail.com>
2010-10-11 12:22:27 -0300

    style fixes

--- lib/jquery_tag/view_helpers.rb
+++ lib/jquery_tag/view_helpers.rb
@@ -1,17 +1,15 @@
 module Jquery
   module Tag
     module ViewHelpers
-
       def jquery_tag(options = {})
         arguments = options.delete(:args) || []
         path = options.delete(:file) || "jquery.js"
         version = options.delete(:version) || "1.4.2"
-
         path = "http://ajax.googleapis.com/ajax/libs/jquery/#{version}/jquery.min.js" if Rails.env.production?
-
         arguments.unshift(path)
-       javascript_include_tag arguments
-        end
+
+        javascript_include_tag arguments
+     end
     end
   end
 end
